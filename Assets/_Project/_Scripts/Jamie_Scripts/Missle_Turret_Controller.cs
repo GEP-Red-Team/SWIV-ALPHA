6c2ba3f55545a8dda2c664dafb483f895651b2ce
Jamie-Winfield <jamie2.winfield@uwe.ac.uk>
2021-02-17 22:22:05 +0000

    added missle turret with tracking missles

---
+++ Assets/_Project/_Scripts/Jamie_Scripts/Missle_Turret_Controller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Missle_Turret_Controller : MonoBehaviour
+{
+    public GameObject missle_prefab = null;
+    public int score_value = 200;
+    public GameObject player = null;
+    public float missle_speed = 10f;
+
+    private Game_Controller game_controller = null;
+    private GameObject missle = null;
+    private bool can_shoot = true;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        game_controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<Game_Controller>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookAtPlayer();
+        if (Input.GetKeyDown("space"))
+        {
+            tryShoot();
+        }
+    }
+    void tryShoot()
+    {
+        if (!missle)
+        {
+            shootMissle();
+        }
+    }
+    void moveWithBackground()
+    {
+        // move turret with the background
+    }
+    
+    void shootMissle()
+    {
+        missle = Instantiate(missle_prefab, transform.position + (transform.forward * 1f), Quaternion.identity);
+        Rigidbody rb = missle.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * missle_speed;
+    }
+
+    void lookAtPlayer()
+    {
+        // float x = player.transform.position.x - transform.position.x;
+        // float y = player.transform.position.y - player.transform.position.y;
+        // float angle = (float)Math.Atan2(y, x);
+        // angle *= Mathf.Rad2Deg;
+        // transform.Rotate(0, angle, 0);
+        // transform.eulerAngles = new Vector3((float)angle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        transform.LookAt(player.transform, Vector3.back);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Bullet") )
+        {
+            // destroy turret and add score
+            game_controller.AddPlayerScore(score_value);
+            Destroy(gameObject);
+        }
+    }
+}
